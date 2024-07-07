33873c677941135a69a638135f281cca000195cd
Silas Della Contrada <me@silenium-dev.net>
2024-07-07 23:53:42 +0200

    set EGL_RENDERABLE_TYPE to EGL_OPENGL_ES3_BIT

--- skiko/src/awtMain/cpp/linux/redrawer.cc
+++ skiko/src/awtMain/cpp/linux/redrawer.cc
@@ -67,7 +67,7 @@ public:
                 EGL_GREEN_SIZE, 8,
                 EGL_BLUE_SIZE, 8,
                 EGL_ALPHA_SIZE, transparency ? 8 : 0,
-                EGL_RENDERABLE_TYPE, EGL_OPENGL_BIT,
+                EGL_RENDERABLE_TYPE, EGL_OPENGL_ES3_BIT,
                 EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
                 EGL_CONFIG_CAVEAT, EGL_NONE,
                 EGL_NONE,
